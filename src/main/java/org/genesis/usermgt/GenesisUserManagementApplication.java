90b5fd67a42a37cae2b44b8b57b7417835706e30
nipunthathsara <nipunthathsara@gmail.com>
2020-12-30 00:33:25 +0530

    Initial commit

---
+++ src/main/java/org/genesis/usermgt/GenesisUserManagementApplication.java
@@ -0,0 +1,13 @@
+package org.genesis.usermgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesisUserManagementApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GenesisUserManagementApplication.class, args);
+    }
+}
---
+++ src/test/java/org/genesis/usermgt/GenesisUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.genesis.usermgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesisUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
